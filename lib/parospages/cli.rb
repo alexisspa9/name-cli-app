10b2315705ec9efbca1b41f9de66223b92f22e74
Alexander <alexisspa9@gmail.com>
2018-03-27 03:55:06 +0300

    building fake data to test on cli

--- lib/parospages/cli.rb
+++ lib/parospages/cli.rb
@@ -1,5 +1,27 @@
 class Parospages::CLI
 	def call
 		puts "Recommended accommodation in Paros Island "	
+		show_list
+		menu
+		goodbye
+	end
+	def show_list
+		puts "test list"	
+	end
+	def menu
+	puts "Enter the number of the accommodation you want more info:"
+		input = nil
+		while input != "exit "
+		input = gets.strip.downcase 
+			case input
+				when "1"
+					puts "more info on 1"
+				when "2"
+					puts "more info on 2"
+			end
+		end
+	end
+	def goodbye
+		puts "See you later! "
 	end
 end
